efaef0e9075bfd2ba5ac24f1c48c84a17654a75f
deepikaganesan <ganesandeepika97@gmail.com>
2019-01-28 20:26:20 +0530

    converting

---
+++ currencyconvert.py
@@ -0,0 +1,25 @@
+Dollars to rupee:
+dollars=int(input("enter the dollars:"))
+rupee=dollars * 71
+print(rupee, "rupee")
+
+Rupees to dollars:
+
+rupee=int(input("enter rupee:"))
+dollars=rupee / 71
+print(dollars, "dollars")
+
+usd to rupee:
+usd=int(input("enter usd:"))
+rupee=usd * 71.16
+print(rupee,"rupee")
+
+rupee to usd:
+rupee=int(input("enter rupee:"))
+usd=rupee / 71.16
+print(usd, "usd")
+
+
+
+
+
